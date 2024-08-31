75d21d8e757018cf6f2b7ac1100908b06af5b60b
Ashen-One666 <2812625268@qq.com>
2024-08-31 16:07:01 +0800

    v0.1

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args){
         Student stu1 = new Student("fizz", 23,
                 "Charming Garden", "NUS");
+        System.out.println("Check git");
     }
 }
